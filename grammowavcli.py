78333d2d0dd5e37fadd9d60bbf5f40e65052ffed
igorkll <frerther@gmail.com>
2026-05-15 11:38:14 +0300

    Update grammowavcli.py

--- grammowavcli.py
+++ grammowavcli.py
@@ -3,17 +3,23 @@ from pythonopenscad import Cube, Sphere
 from pythonopenscad.m3dapi import M3dRenderer
 import argparse
 
-parser = argparse.ArgumentParser(
+# --------------------------------------- parsing cli arguments
+
+argsparser = argparse.ArgumentParser(
     prog="grammowavcli",
     description="converts a music file into a 3D model of a record for printing on a 3D printer and listening on a gramophone"
 )
 
-parser.add_argument("file")
-parser.add_argument("-o", "--output", default="out.stl")
-parser.add_argument("--diameter", type=int, default=120)
-parser.add_argument("--rpm", type=int, default=78)
+argsparser.add_argument("file")
+argsparser.add_argument("-o", "--output", default="out.stl")
+argsparser.add_argument("--diameter", type=int, default=120)
+argsparser.add_argument("--rpm", type=int, default=78)
 
-args = parser.parse_args()
+args = argsparser.parse_args()
+
+
+
+# --------------------------------------- 
 
 model = Cube([20,20,20]) - Sphere(r=12)
 
