385848821d8083a1052bf4697894aea25e9ee4ff
igorkll <frerther@gmail.com>
2026-05-15 11:42:29 +0300

    Update grammowavcli.py

--- grammowavcli.py
+++ grammowavcli.py
@@ -10,18 +10,20 @@ argsparser = argparse.ArgumentParser(
     description="converts a music file into a 3D model of a record for printing on a 3D printer and listening on a gramophone"
 )
 
-argsparser.add_argument("file")
+argsparser.add_argument("input")
 argsparser.add_argument("-o", "--output", default="out.stl")
 argsparser.add_argument("--diameter", type=int, default=120)
 argsparser.add_argument("--rpm", type=int, default=78)
 
 args = argsparser.parse_args()
 
+# --------------------------------------- parsing music file
 
 
-# --------------------------------------- 
+
+# ---------------------------------------
 
 model = Cube([20,20,20]) - Sphere(r=12)
 
 mesh = model.renderObj(M3dRenderer())
-mesh.write_solid_stl("out.stl")
+mesh.write_solid_stl(args.output)
